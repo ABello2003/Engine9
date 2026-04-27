394024903d3aa81fff3276f89f3ebbafaf0b3250
ABello2003 <aib5759@psu.edu>
2026-04-27 07:45:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from lexer import analyze_tokens
 from interpreter import Engine9Interpreter
 
-SOURCE_FILE = "program.e9"
-
 def main():
-    print("===== ENGINE9 TOKEN REPORT =====")
-    analyze_tokens(SOURCE_FILE)
+    print("===== Engine9 Interactive Mode =====")
+    print("Type Engine9 code below.")
+    print("Type 'run' to execute.")
+    print("Type 'exit' to quit.\n")
 
-    print("\n===== ENGINE9 PROGRAM OUTPUT =====")
-    interpreter = Engine9Interpreter()
-    interpreter.run_file(SOURCE_FILE)
+    lines = []
 
-    print("\n===== SYMBOL TABLE =====")
-    interpreter.symbol_table.print_table()
+    while True:
+        line = input("E9> ")
+
+        if line.lower() == "exit":
+            print("Goodbye.")
+            break
+
+        if line.lower() == "run":
+            interpreter = Engine9Interpreter()
+            print("\n===== PROGRAM OUTPUT =====")
+            interpreter.run_lines(lines)
+
+            print("\n===== SYMBOL TABLE =====")
+            interpreter.symbol_table.print_table()
+
+            lines = []
+            print("\nYou can type a new Engine9 program now.\n")
+            continue
+
+        lines.append(line)
 
 if __name__ == "__main__":
     main()
