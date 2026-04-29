abec43b0a0953c26d8e91b94bdaee09d1e9bcda3
ABello2003 <aib5759@psu.edu>
2026-04-29 14:37:07 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,23 @@
+def force(m, a):
+    return m * a
 
+def kinetic_energy(m, v):
+    return 0.5 * m * v * v
+
+def ohm_current(v, r):
+    return v / r
+
+def work(f, d):
+    return f * d
+
+def power(w, t):
+    return w / t
+
+def pressure(f, a):
+    return f / a
+
+def density(m, v):
+    return m / v
+
+def momentum(m, v):
+    return m * v
