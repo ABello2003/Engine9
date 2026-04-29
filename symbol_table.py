07998da4b28073f938d3cdc3643b468e989f372b
ABello2003 <aib5759@psu.edu>
2026-04-29 14:36:49 -0400

    Update symbol_table.py

--- symbol_table.py
+++ symbol_table.py
@@ -2,26 +2,20 @@ class SymbolTable:
     def __init__(self):
         self.table = {}
 
-    def add_or_update(self, name, value):
+    def set(self, name, value):
         if isinstance(value, int):
-            data_type = "int"
+            t = "int"
         elif isinstance(value, float):
-            data_type = "float"
+            t = "float"
         else:
-            data_type = "unknown"
+            t = "unknown"
 
-        self.table[name] = {
-            "type": data_type,
-            "value": value
-        }
+        self.table[name] = {"type": t, "value": value}
 
-    def get_value(self, name):
-        if name not in self.table:
-            raise NameError(f"Variable '{name}' is not defined.")
+    def get(self, name):
         return self.table[name]["value"]
 
     def print_table(self):
-        print("Variable\tType\tValue")
-        print("-----------------------------")
-        for name, info in self.table.items():
-            print(f"{name}\t\t{info['type']}\t{info['value']}")
+        print("\nSYMBOL TABLE")
+        for k, v in self.table.items():
+            print(f"{k} → {v}")
