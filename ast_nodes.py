193b2abc6582fb7c9e255bf657f250ff63b2dfce
ABello2003 <aib5759@psu.edu>
2026-04-29 14:36:21 -0400

    Update ast_nodes.py

--- ast_nodes.py
+++ ast_nodes.py
@@ -1,1 +1,13 @@
+class LetNode:
+    def __init__(self, name, expression):
+        self.name = name
+        self.expression = expression
 
+class PrintNode:
+    def __init__(self, value):
+        self.value = value
+
+class FuncCallNode:
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
