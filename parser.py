5331323c489215c426852b30eb51c04811d5fab2
ABello2003 <aib5759@psu.edu>
2026-04-29 14:36:07 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,20 @@
+from ast_nodes import LetNode, PrintNode, FuncCallNode
 
+def parse(tokens):
+    ast = []
+    i = 0
+
+    while i < len(tokens):
+        if tokens[i] == "let":
+            name = tokens[i+1]
+            expr = tokens[i+3:]
+            ast.append(LetNode(name, expr))
+            break
+
+        elif tokens[i] == "print":
+            ast.append(PrintNode(tokens[i+1]))
+            break
+
+        i += 1
+
+    return ast
